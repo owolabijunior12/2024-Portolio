b9044cc942ce5036c410de66825f07088657aea1
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2024-03-02 20:44:29 +0100

    small changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -38,19 +38,7 @@ const Navbar = () => {
               Blogs
             </a>
           </div>
-        </div>
-
-        {/* <div className="flex flex-row gap-5">
-          {Socials.map((social) => (
-            <Image
-              src={social.src}
-              alt={social.name}
-              key={social.name}
-              width={24}
-              height={24}
-            />
-          ))}
-        </div> */}
+        </div>      
       </div>
     </div>
   );
